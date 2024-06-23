caf7ba9ac3d6b0d019ce11045c70fea931e73428
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-23 14:19:53 +0100

    Created different way to read - readFileBuff - using duffer 5byte elements

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"math/rand"
 	"os"
 )
@@ -52,11 +54,37 @@ func readFile(filename string) {
 	fmt.Printf(string(fileContent))
 }
 
+func readFileBuff(filename string) {
+
+	buff := make([]byte, 5)
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Помилка відкриття файлу", err)
+	}
+
+	for {
+		n, err := file.Read(buff)
+		if err != nil {
+
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Println("помилка відкриття буферу та читання файла")
+		}
+
+		fmt.Println(n, "\"", string(buff), "\"")
+	}
+}
+
 func main() {
 	const filename = "a.txt"
 	err := createFile(filename)
 	if err != nil {
 		fmt.Printf("помилка", err)
 	}
-	readFile(filename)
+
+	readFileBuff(filename)
+
 }
